d61c4625cb05708215df615ed44ccd69e3f3faba
narrow05 <narrow05@gmail.com>
2019-02-13 21:26:01 -0300

    walter

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,48 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var uno
+    var dos
+    var tres
+    var suma
+      uno=document.getElementById("PrecioUno").value;
+      dos=document.getElementById("PrecioDos").value;
+      tres= document.getElementById("PrecioTres").value;
+      uno=parseInt(uno);
+      dos=parseInt(dos);
+      tres=parseInt(tres);
+      suma=uno+dos+tres;
+      alert("la suma "+suma)
 }
 function Promedio () 
 {
+    var uno
+    var dos
+    var tres
+    var promedio
+      uno=document.getElementById("PrecioUno").value;
+      dos=document.getElementById("PrecioDos").value;
+      tres= document.getElementById("PrecioTres").value;
+      uno=parseInt(uno);
+      dos=parseInt(dos);
+      tres=parseInt(tres);
+      promedio=(uno+dos+tres)/3;
+      alert("el promedio es "+promedio)
 	
 }
 function PrecioFinal () 
 {
+    var uno
+    var dos
+    var tres
+    var precio
+      uno=document.getElementById("PrecioUno").value;
+      dos=document.getElementById("PrecioDos").value;
+      tres= document.getElementById("PrecioTres").value;
+      uno=parseInt(uno);
+      dos=parseInt(dos);
+      tres=parseInt(tres);
+      precio=(uno+dos+tres)*1.21;
+      alert("el promedio es "+precio)
 	
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
+    ancho=prompt(""); 
+    largo=prompt("");
+    ancho=parseInt(ancho);
+    largo=parseInt(largo);
+    perimetro=(largo*2) + (ancho*2);
+    alert(perimetro)
 
 }
