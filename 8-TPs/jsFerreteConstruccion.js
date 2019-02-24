625ada2841411c90b0dc6e8e26a50d68e74df1fe
narrow05 <47463700+narrow05@users.noreply.github.com>
2019-02-24 19:10:37 -0300

    Update jsFerreteConstruccion.js

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,46 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+    var Largo;
+    var Ancho; 
+    var perimetro;
+    var alambreTotal;
+    Largo=document.getElementById("Largo").value;
+    Ancho=document.getElementById("Ancho").value;
+    Largo=parseInt(Largo);
+    Ancho=parseInt(Ancho);
+    perimetro=Largo*2 + Ancho*2;
+    alambreTotal=perimetro*3;
+    
+    alert("el perimetro es "+perimetro+" y la cantidad de alambre a comprar para darle tres vueltas es "+alambreTotal );
 }
 function Circulo () 
-{
-	
+{  
+    var Radio;
+    var radioTotal;
+    var alambreRadio;
+    Radio=document.getElementById("Radio").value;
+    Radio=parseInt(Radio);
+    radioTotal=Radio*2*3.14;
+    alambreRadio=radioTotal*3;
+    alert("el perimetro del circulo es "+radioTotal+" y la cantidad de alambre a comprar para darle tres vueltas es "+alambreRadio );	
 }
 function Materiales () 
 {
-	
+	var Largo;
+    var Ancho; 
+    var superficie;
+    var bolsadecemetoporm2=2;
+    var bolsadecalporm2=3;
+    Largo=document.getElementById("Largo").value;
+    Ancho=document.getElementById("Ancho").value;
+    Largo=parseInt(Largo);
+    Ancho=parseInt(Ancho);
+    superficie=Largo* Ancho;
+    bolsadecemetoporm2=superficie/bolsadecemetoporm2;
+    bolsadecalporm2=superficie/bolsadecalporm2;
+    
+    
+    alert("La superficie total es "+superficie+ " y se necesitan para hacer un conta piso "+bolsadecemetoporm2+ "bolsas de cemento y "+bolsadecalporm2+" bolsas de cal")
+
 }
